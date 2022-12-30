5f8cf2dfeb672ed2f83b92a8be60a75f09ef9d36
yeongjae <yeongjae9501@gmail.com>
2022-12-30 19:29:09 +0900

    font-size 수정

--- js/main.js
+++ js/main.js
@@ -96,6 +96,23 @@ $(function () {
         let section = $('.section');
         console.log('dd', section.height());
       }
+      let text1 = document.querySelector('.section2-title');
+
+      let observer = new IntersectionObserver((e) => {
+        console.log('E', e);
+
+        e.forEach((박스) => {
+          if (박스.isIntersecting) {
+            박스.target.style.top = 0;
+            박스.target.style.opacity = 1;
+          } else {
+            박스.target.style.opacity = 0;
+            박스.target.style.top = 100 + '%';
+          }
+        });
+      });
+
+      observer.observe(text1);
     });
 
     // -------------window 가로 2000이하일떄
